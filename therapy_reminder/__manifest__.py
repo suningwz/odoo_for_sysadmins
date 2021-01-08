41919cb60493e946eb116e2e83177e68903c9890
Raistdev <juanmadevelop@gmail.com>
2021-01-08 16:32:19 +0100

    First commit in 13 devel branch

---
+++ therapy_reminder/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    'name': 'Therapy reminder',
+    'version': '13.0.1.0.0',
+    'summary': '''This app allows to set the personal therapy and 
+                get notifications to take it''',
+    'category': 'Health',
+    'author': 'Raistdev',
+    'maintainer': 'Raistdev',
+    'website': 'https://raist.dev',
+    'license': 'AGPL-3',
+    'contributors': [
+        '',
+    ],
+    'depends': [
+        '',
+    ],
+    'external_dependencies': {
+        'python': [
+            '',
+        ],
+    },
+    'data': [
+        '',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
